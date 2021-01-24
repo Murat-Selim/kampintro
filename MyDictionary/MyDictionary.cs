36bd309b4939ea3a480266da4103b14386d2d175
murat salımov <m.murat1994@hotmail.com>
2021-01-24 18:44:04 +0300

    Odev

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<intKey, stringValue>
+    {
+        int[] keyArray;
+        string[] valueArray;
+        int[] tempKeyArray;
+        string[] tempValueArray;
+
+        public MyDictionary()
+        {
+            keyArray = new int[0];
+            valueArray = new string[0];
+        }
+
+        public void Add(int key, string value)
+        {
+            tempKeyArray = keyArray;
+            tempValueArray = valueArray;
+
+            keyArray = new int[keyArray.Length + 1];
+            valueArray = new string[valueArray.Length + 1];
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                keyArray[i] = tempKeyArray[i];
+            }
+            keyArray[keyArray.Length - 1] = key;
+
+            for (int i = 0; i < tempValueArray.Length; i++)
+            {
+                valueArray[i] = tempValueArray[i];
+            }
+            valueArray[valueArray.Length - 1] = value;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Ali");
+            myDictionary.Add(2, "Ayse");
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
